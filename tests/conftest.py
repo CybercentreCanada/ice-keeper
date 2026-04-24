535fd74a14846d1826c8cb1c6fa702429b8fcb57
cccs-jc <jean-claude.cote@cyber.gc.ca>
2026-04-24 15:21:57 +0000

    disable logging when testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,6 @@ import os
 
 import pytest
 
-from ice_keeper import configure_logger
 from ice_keeper.config import ICEKEEPER_CONFIG, Config
 
 
@@ -11,4 +10,3 @@ def reset_config() -> None:
     config_file = os.environ.get(ICEKEEPER_CONFIG)
     assert config_file is not None
     Config.load_config(config_file)
-    configure_logger()
