5831a3a4977f7fcd8d259f8aa273a0e3ece15f21
cccs-jc <jean-claude.cote@cyber.gc.ca>
2026-05-14 13:30:25 +0000

    show more partitions

--- ice_keeper/task/action/optimization/optimization.py
+++ ice_keeper/task/action/optimization/optimization.py
@@ -142,7 +142,7 @@ class OptimizationStrategy(ActionStrategy):
             widening_rule = self.get_widening_rule(spec_id)
             # Collect partition summary for the spec_id
             summary = PartitionSummary(self.mnt_props, spec_id, widening_rule)
-            summary.show(100)
+            summary.show(10000)
 
             try:
                 # Diagnose the partitions for optimization opportunities
