15711fa18c735493ea4f348cfb245193566a78fd
cccs-jc <jean-claude.cote@cyber.gc.ca>
2026-06-26 15:33:22 +0000

    fix spec id validation

--- ice_keeper/task/action/optimization/optimization.py
+++ ice_keeper/task/action/optimization/optimization.py
@@ -289,7 +289,10 @@ class OptimizationStrategy(ActionStrategy):
 
         # Validate that all spec IDs exist in the metadata
         for spec_id in unique_spec_ids:
-            if spec_id < 0 or spec_id >= len(self.mnt_props.partition_specs.get_specifications()):
+            for spec in self.mnt_props.partition_specs.get_specifications():
+                if spec.spec_id == spec_id:
+                    break
+            else:
                 msg = f"Partition spec id found: {spec_id} does not exist in the list of partition specs in metadata."
                 raise Exception(msg)
         return unique_spec_ids
