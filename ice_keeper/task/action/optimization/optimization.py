d3cfe167520a51370dc380f20ca4dacebe25cb3c
cccs-jc <jean-claude.cote@cyber.gc.ca>
2026-06-26 15:40:32 +0000

    code review fix

--- ice_keeper/task/action/optimization/optimization.py
+++ ice_keeper/task/action/optimization/optimization.py
@@ -289,12 +289,11 @@ class OptimizationStrategy(ActionStrategy):
 
         # Validate that all spec IDs exist in the metadata
         for spec_id in unique_spec_ids:
-            for spec in self.mnt_props.partition_specs.get_specifications():
-                if spec.spec_id == spec_id:
-                    break
-            else:
+            try:
+                self.mnt_props.partition_specs[spec_id]
+            except KeyError as key_error:
                 msg = f"Partition spec id found: {spec_id} does not exist in the list of partition specs in metadata."
-                raise Exception(msg)
+                raise Exception(msg) from key_error
         return unique_spec_ids
 
     def _sorted_column_exists(self, column: str) -> bool:
